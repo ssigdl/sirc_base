b9a00b0a782b3d2bd0e380f0eb46c942710fc749
ssigdl <domotrix@hotmail.com>
2014-08-21 17:28:54 -0500

    Pruebas

---
+++ src/main/java/com/ssigdl/sirc/web/Pruebas.java
@@ -0,0 +1,17 @@
+package com.ssigdl.sirc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Pruebas {
+	@RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
+	
+	public String hello(ModelMap model) { 
+		model.addAttribute("name", "JCG Hello World!");
+		return "pruebas";
+	}
+	
+}
